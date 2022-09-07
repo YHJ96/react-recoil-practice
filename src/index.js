a27a79435427b60964035480fbe9873e9baa7bf4
YHJ96 <yhj878812@naver.com>
2022-09-08 01:46:37 +0900

    feat: RecoilRoot 설정 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { RecoilRoot } from 'recoil';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
+);
