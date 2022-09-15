4d3347f6d6393078640791c80404f931bb128dd9
YHJ96 <yhj878812@naver.com>
2022-09-16 00:52:04 +0900

    feat: Store 컴포넌트 추가 완료

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import Store from './Store';
 import { RecoilRoot } from 'recoil';
 
 const root = document.getElementById('root');
 ReactDOM.createRoot(root).render(
   <RecoilRoot>
-    <App />
+    <Store />
   </RecoilRoot>,
 );
